2b56dec23f67c28287c63e4317f73f218d7b8f11
Deniel Bayron <dpbayron@gmail.com>
2024-01-24 14:13:20 +0800

    Add link to Tindog Project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -12,7 +12,7 @@ const Projects = () => {
     <h1 className="text-4xl font-bold text-center py-12 text-[#001b5e]">Projects</h1>
     
     <div className="grid sm:grid-cols-2 gap-12 ">
-        <ProjectItem img={teraImg} title='Tindog' code='HTML & CSS (BootStrap)' link='' />
+        <ProjectItem img={teraImg} title='Tindog' code='HTML & CSS (BootStrap)' link='https://dazzling-piroshki-ea4cc4.netlify.app' />
         <ProjectItem img={tera2Img} title='Teracotta Shop' code='React JS' link='https://github.com/DnielBayron/Teracotta-Shop' />
         <ProjectItem img={tera3Img} title='Teracotta Mail' code='Express JS & MailChimp' link='https://github.com/DnielBayron/teracotta-server' />
         <ProjectItem img={tera4Img} title='Personal Website' code='HTML and CSS'  link='https://github.com/DnielBayron/My-First-Site' />
